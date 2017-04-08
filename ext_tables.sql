ebed8d925d6b1f020da88e0dff1a634e05044629
Pascal Mayer <typo3@bsdist.ch>
2017-04-08 19:36:24 +0200

    new caption field, image* fields removed

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,8 +2,5 @@
 # Re-add fields removed in 7.x
 #
 CREATE TABLE tt_content (
-
-  image_link text,
-  imagecaption text,
-  imagecaption_position varchar(6) NOT NULL DEFAULT ''
+  tx_videoce_caption text
 );
