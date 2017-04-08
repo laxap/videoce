b419723c7ae02c6fe3eff509c8380f5abd59634d
Pascal Mayer <typo3@bsdist.ch>
2017-04-08 19:32:44 +0200

    not needed labels removed

--- Configuration/TypoScript/tsconfig.ts
+++ Configuration/TypoScript/tsconfig.ts
@@ -1,48 +1,20 @@
-
-# --- Add new content element ---
-#
-mod.wizards.newContentElement.wizardItems.special.elements.videoce {
-    icon = ../typo3conf/ext/videoce/Resources/Public/Icons/wizard_videocontent.png
-    title = LLL:EXT:videoce/Resources/Private/Language/locallang_db.xlf:video.ctype.title
-    description = LLL:EXT:videoce/Resources/Private/Language/locallang_db.xlf:video.ctype.description
-    tt_content_defValues {
-        CType = videoce_videocontent
+# add new content element
+mod.wizards.newContentElement.wizardItems.special {
+    elements {
+        videoce {
+            iconIdentifier = tx-videocontent-wizard
+            title = LLL:EXT:videoce/Resources/Private/Language/locallang_db.xlf:video.ctype.title
+            description = LLL:EXT:videoce/Resources/Private/Language/locallang_db.xlf:video.ctype.description
+            tt_content_defValues {
+                CType = videoce_videocontent
+            }
+        }
     }
+    show := addToList(videoce)
 }
-mod.wizards.newContentElement.wizardItems.special.show := addToList(videoce)
 
-
-
-# --- Rename labels (from image to video) ---
-#
+# change fields for videoce content element
 TCEFORM.tt_content {
-    image_link {
-        types {
-            videoce_videocontent.label = LLL:EXT:videoce/Resources/Private/Language/locallang_db.xlf:video.links
-        }
-    }
-
-    imagecaption {
-        types {
-            videoce_videocontent.label = LLL:EXT:videoce/Resources/Private/Language/locallang_db.xlf:video.caption
-        }
-    }
-
-    imagewidth {
-        types {
-            videoce_videocontent.label = LLL:EXT:videoce/Resources/Private/Language/locallang_db.xlf:video.width
-        }
-    }
-    imageheight {
-        types {
-            videoce_videocontent.label = LLL:EXT:videoce/Resources/Private/Language/locallang_db.xlf:video.height
-        }
-    }
-    imagecaption_position {
-        types {
-            videoce_videocontent.label = LLL:EXT:videoce/Resources/Private/Language/locallang_db.xlf:video.position
-        }
-    }
     imagecols {
         types {
             videoce_videocontent {
@@ -51,4 +23,3 @@ TCEFORM.tt_content {
         }
     }
 }
-
