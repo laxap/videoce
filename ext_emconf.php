dd0c2e0de41c5bda074b3f3d77d5957ee9bb31f2
Pascal Mayer <typo3@bsdist.ch>
2017-04-08 19:36:42 +0200

    Release 0.9.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,31 +4,30 @@
  * Extension Manager/Repository config file for ext: "videoce"
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
 	'title' => 'Video Content Element',
 	'description' => 'Another content element for Youtube, Vimeo and Dailymotion videos.',
 	'category' => 'fe',
 	'author' => 'Pascal Mayer',
 	'author_email' => 'typo3@bsdist.ch',
 	'author_company' => '',
-	'shy' => '',
-	'version' => '0.8.2',
-	'priority' => '',
-	'module' => '',
+	'version' => '0.9.0',
 	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => '0',
 	'createDirs' => '',
-	'modify_tables' => '',
+	'modify_tables' => 'tt_content',
 	'clearCacheOnLoad' => 1,
-	'lockType' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '6.2.0-7.6.99',
-		),
-		'conflicts' => array(
-		),
-	),
-);
+	'constraints' => [
+		'depends' => [
+            'typo3' => '8.7.0-8.7.99',
+            'fluid_styled_content' => '8.7.0-8.7.99',
+		],
+		'conflicts' => [],
+	],
+    'autoload' => [
+        'psr-4' => ['Laxap\\Videoce\\' => 'Classes']
+    ],
+];
 
 ?>
