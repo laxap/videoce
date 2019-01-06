41b10bac40a0e64c2b2da3519ba5941ddf67a799
Pascal Mayer <typo3@bsdist.ch>
2019-01-06 16:49:12 +0100

    new version for TYPO3 v9.5

--- ext_emconf.php
+++ ext_emconf.php
@@ -9,9 +9,9 @@ $EM_CONF[$_EXTKEY] = [
 	'description' => 'Another content element for Youtube, Vimeo and Dailymotion videos.',
 	'category' => 'fe',
 	'author' => 'Pascal Mayer',
-	'author_email' => 'typo3@bsdist.ch',
+	'author_email' => 'typo3@lascap.ch',
 	'author_company' => '',
-	'version' => '0.9.0',
+	'version' => '0.10.0',
 	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => '0',
@@ -20,8 +20,7 @@ $EM_CONF[$_EXTKEY] = [
 	'clearCacheOnLoad' => 1,
 	'constraints' => [
 		'depends' => [
-            'typo3' => '8.7.0-8.7.99',
-            'fluid_styled_content' => '8.7.0-8.7.99',
+            'typo3' => '9.5.0-9.5.99'
 		],
 		'conflicts' => [],
 	],
